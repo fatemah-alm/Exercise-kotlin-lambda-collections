474af8aa019c7f0dad61a0cff6e8b5da81b1e307
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-13 16:21:29 +0300

    Part 1

---
+++ lambdaCollections/src/Main.kt
@@ -0,0 +1,18 @@
+import java.awt.List
+import javax.print.DocFlavor.STRING
+
+fun main() {
+
+    // part 1
+    val numbers = listOf(1,2,3,4,5,6,7,8,9,10)
+    val doubledList = numbers.map{ it*2 }
+    println(doubledList)
+    
+}
+
+
+
+
+//fun filteringA (name:String):Boolean{
+//    return name.startsWith('A')
+//}
